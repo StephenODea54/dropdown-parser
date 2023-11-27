0da3220c6bdbb03addc282342dce7389fc485095
StephenODea54 <odeastephen1@gmail.com>
2023-11-27 13:52:21 -0500

    Update CLI function

--- dropdown_parser/cli.py
+++ dropdown_parser/cli.py
@@ -1,5 +1,20 @@
-def cli():
-    print("Hello world!")
+import click
+from dropdown_parser import Parser, OutputTypes
+from typing import get_args
+
+
+@click.command()
+@click.argument("file_path", required=True, type=str)
+@click.option(
+    "--output",
+    "-o",
+    default="csv",
+    type=click.Choice(get_args(OutputTypes)),
+    show_default=True,
+)
+def cli(file_path: str, output: OutputTypes = "csv") -> None:
+    parser = Parser(file_path)
+    parser.parse(output)
 
 
 if __name__ == "__main__":
